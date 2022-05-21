4d1f616a7fe4c9f5c50a1175017909020fdc28f8
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-05-21 11:52:17 -0300

    MoveJogador

--- Assets/Scripts/MoveJogador.cs
+++ Assets/Scripts/MoveJogador.cs
@@ -1,28 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MoveJogador : MonoBehaviour
 {
-    public float Velocidade = 10;
-    private float eixoX;
-    private float eixoZ;
-    
-    direcao = new Vector3(eixoX, 0, eixoZ);
-    
-    void Update()
-    {
-        //Inputs do Jogador - Guardando teclas apertadas
-        eixoX = Input.GetAxis("Horizontal");
-        eixoZ = Input.GetAxis("Vertical");
-    }
+    [SerializeField] public float aceleracao = 10.0f;
+    public float velocidadeMax = 50.0f;
+    public Rigidbody2D jogadorRb;
+
 
     void FixedUpdate()
-    {   //Movimentacao do Jogador por segundo
-        GetComponent<Rigidbody>().MovePosition
-            (GetComponent<Rigidbody>().position +
-            (direcao * Velocidade * Time.deltaTime));
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Vector3 direcao = Vector3.left * aceleracao;            //Vector3 direcao = new Vector3 (0.0f, 1.0f * aceleracao, 0.0f);
+            jogadorRb.AddForce(direcao, ForceMode2D.Force);         //Adiciona aceleração no jogadorRb
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Vector3 direcao = Vector3.right * aceleracao;
+            jogadorRb.AddForce(direcao, ForceMode2D.Force); 
+        
+        }
+        if(jogadorRb.velocity.magnitude > velocidadeMax)
+        {
+            jogadorRb.velocity = Vector2.ClampMagnitude(jogadorRb.velocity, velocidadeMax);//velocidade atual com o limite de velocidade
+        }
+
 
     }
+    
 }
