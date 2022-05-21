bb51e0f20b6be6a2f222a7d8af62052f8c05d710
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-05-21 12:16:45 -0300

    MoveCenario

--- Assets/Scripts/MoveCenario.cs
+++ Assets/Scripts/MoveCenario.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class MoveCenario : MonoBehaviour
 {
-    public Rigidbody2D cenario;
     [SerializeField] public float aceleracao = 5.0f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 direcao = transform.up * aceleracao;            //Vector3 direcao = new Vector3 (0.0f, 1.0f * aceleracao, 0.0f);
-            cenario.AddForce(direcao, ForceMode2D.Force);
+         transform.position = transform.position + new Vector3(0.0f,  aceleracao * Time.deltaTime, 0.0f);    
     }
 }
