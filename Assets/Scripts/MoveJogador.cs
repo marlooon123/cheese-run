e24735f8b5adf1e774c3408b11e11942607cac5f
Gustavo <gustavofelix01@yahoo.com>
2022-06-01 18:31:56 -0300

    Adicionando text ui e script TMP

--- Assets/Scripts/MoveJogador.cs
+++ Assets/Scripts/MoveJogador.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoveJogador : MonoBehaviour
 {
@@ -10,13 +11,22 @@ public class MoveJogador : MonoBehaviour
 
     private bool Vivo = true;
 
+    public GameObject loseTextObject;
+    public GameObject winTextObject;
+    private int count;
+
+    void Start()
+    {
+        loseTextObject.SetActive(false);
+        winTextObject.SetActive(false);
+    }
 
     void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.A))
         {
             Vector3 direcao = Vector3.left * aceleracao;            //Vector3 direcao = new Vector3 (0.0f, 1.0f * aceleracao, 0.0f);
-            jogadorRb.AddForce(direcao, ForceMode2D.Force);         //Adiciona aceleração no jogadorRb
+            jogadorRb.AddForce(direcao, ForceMode2D.Force);         //Adiciona aceleracao no jogadorRb
         }
         if(Input.GetKey(KeyCode.D))
         {
@@ -28,8 +38,21 @@ public class MoveJogador : MonoBehaviour
         {
             jogadorRb.velocity = Vector2.ClampMagnitude(jogadorRb.velocity, velocidadeMax);//velocidade atual com o limite de velocidade
         }
+    }
 
-
+    bool marca = false;
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Vitoria") & marca == false)
+        {
+            winTextObject.SetActive(true);
+            marca = true;
+        }
+        if(other.gameObject.CompareTag("Derrota") & marca == false)
+        {
+            loseTextObject.SetActive(true);
+            marca = true;
+        }
     }
     
 }
